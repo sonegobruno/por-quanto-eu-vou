f0bb67bdce299f308cee8d4afb94e8860340f94a
Bruno <sonego.bruno@gmail.com>
2026-04-16 20:45:59 -0300

    refactor: remove default code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'pqev-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
