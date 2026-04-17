1fbad67a9dccf20d69b3e9d107afea0cd4bd91d7
Bruno <sonego.bruno@gmail.com>
2026-04-16 21:03:19 -0300

    feat(home): add component page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'pqev-root',
   templateUrl: './app.html',
+  imports: [RouterOutlet],
 })
 export class App {}
