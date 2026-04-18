7e610122dbda97c7b4fd97f52fda533f1dc29ee0
Bruno <sonego.bruno@gmail.com>
2026-04-17 23:36:40 -0300

    chore: config providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  ErrorHandler,
+  LOCALE_ID,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { logProviders } from './core/services/log/log-providers';
+import { ErrorHandlerService } from './core/services/error-handler/error-handler.service';
+import { DatePipe, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    ...logProviders,
+    { provide: ErrorHandler, useClass: ErrorHandlerService },
+    DatePipe,
+    { provide: LOCALE_ID, useValue: 'pt' },
+  ],
 };
