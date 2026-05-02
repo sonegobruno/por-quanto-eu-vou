5bd75065b6e96ccf373641988205689598e7c96e
Bruno <sonego.bruno@gmail.com>
2026-05-02 11:45:20 -0300

    test: fix tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,11 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'pqev-header',
+  template: '',
+})
+class MockHeaderComponent {}
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-    }).compileComponents();
+    })
+      .overrideComponent(App, {
+        set: {
+          imports: [RouterOutlet, MockHeaderComponent],
+        },
+      })
+      .compileComponents();
   });
 
   it('should create the app', () => {
--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { ThemeService } from '@app/shared/services/theme/theme.service';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -9,6 +10,15 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HeaderComponent],
+      providers: [
+        {
+          provide: ThemeService,
+          useValue: {
+            isLightMode: vi.fn(),
+            toggleTheme: vi.fn(),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
