89c25cb3853e127cf184feddb18be31728f5d138
Bruno <sonego.bruno@gmail.com>
2026-05-14 22:27:20 -0300

    feat(cookie): add expire date default

--- src/app/shared/services/cookie/cookie.service.spec.ts
+++ src/app/shared/services/cookie/cookie.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CookieService } from './cookie.service';
+import { now } from '@app/shared/utils/date/date-utils';
+import { addMonths } from 'date-fns';
+import { CookieService as NgxCookieService } from 'ngx-cookie-service';
 
 describe('CookieService', () => {
   let service: CookieService;
@@ -10,7 +13,7 @@ describe('CookieService', () => {
       providers: [
         CookieService,
         {
-          provide: CookieService,
+          provide: NgxCookieService,
           useValue: { get: vi.fn(), set: vi.fn() } as Pick<CookieService, 'get' | 'set'>,
         },
       ],
@@ -23,7 +26,7 @@ describe('CookieService', () => {
   });
 
   it('should call get method of cookie service', () => {
-    const cookieService = TestBed.inject(CookieService);
+    const cookieService = TestBed.inject(NgxCookieService);
     const cookieName = 'testCookie';
 
     service.get(cookieName);
@@ -31,13 +34,24 @@ describe('CookieService', () => {
     expect(cookieService.get).toHaveBeenCalledWith(cookieName);
   });
 
-  it('should call set method of cookie service', () => {
-    const cookieService = TestBed.inject(CookieService);
+  it('should call set method of cookie service with expiration default', () => {
+    const cookieService = TestBed.inject(NgxCookieService);
     const cookieName = 'testCookie';
     const cookieValue = 'testValue';
 
     service.set(cookieName, cookieValue);
 
-    expect(cookieService.set).toHaveBeenCalledWith(cookieName, cookieValue);
+    expect(cookieService.set).toHaveBeenCalledWith(cookieName, cookieValue, addMonths(now(), 6));
+  });
+
+  it('should call set method of cookie service with expiration', () => {
+    const cookieService = TestBed.inject(NgxCookieService);
+    const cookieName = 'testCookie';
+    const cookieValue = 'testValue';
+    const expiredDate = new Date('2024-12-31');
+
+    service.set(cookieName, cookieValue, expiredDate);
+
+    expect(cookieService.set).toHaveBeenCalledWith(cookieName, cookieValue, expiredDate);
   });
 });
